55e68d4dd078766ae0bd0a1a255e28105dc4c1cc
vgsnv <vgsnv@yandex.ru>
2017-10-27 22:25:28 +0300

    Добавляет первый компонент react

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
-console.info('Hello world!')
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = (props) => {
+  return (<h1>Hello, {props.name}</h1>)
+};
+
+ReactDOM.render(<App name={'World'} />, document.getElementById('root'));
