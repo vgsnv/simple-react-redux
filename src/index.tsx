f6d87963ab8451b9b2f4f97a3d3a977b324e4f2c
vgsnv <vgsnv@yandex.ru>
2017-11-18 22:45:55 +0300

    Добавляет ColorButton

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,59 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const Button = (props) => {
-  return (<button
-    onClick={props.onClick}
-  >
-    {props.title}
-  </button>)
+interface Props {
+  onClick: () => void;
+  title: string;
+  color: string;
+}
+
+interface State {
+}
+
+class ColorButton extends React.Component<Props, State> {
+
+  render() {
+
+    const {
+      onClick,
+      title,
+      color
+    } = this.props;
+
+    return (<div
+      style={{
+        width: 150,
+      }}
+    >
+      <p
+        style={{
+          textAlign: 'center',
+        }}
+      >{title}</p>
+      <button
+        style={{
+          backgroundColor: color,
+          borderRadius: 50,
+          height: 100,
+          width: 100,
+          margin: '0, auto, 0, auto',
+        }}
+        onClick={onClick}
+      >
+      </button>
+    </div>)
+  }
+
 }
 
 const App = (props) => {
   return (<div>
-    <Button
-      title='Добавить'
+    <ColorButton
+      title='Красный'
       onClick={() => console.info('Click')}
+      color='red'
     />
   </div>)
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
