65967337a41f1f08ee4a4b90e5a1a468f0736288
vgsnv <vgsnv@yandex.ru>
2017-11-19 10:46:27 +0300

    Добавляет массив цветов

--- src/components/ColorsCode.tsx
+++ src/components/ColorsCode.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 interface Props {
   clickDelBtn: () => void;
-  color: string;
+  colors: Array<string>;
 }
 
 interface State {
@@ -10,27 +10,37 @@ interface State {
 
 export default class ColorsCode extends React.Component<Props, State> {
 
-  render() {
+  private renderColorCircle() {
 
     const {
       clickDelBtn,
-      color
+      colors
     } = this.props;
+
+    return colors.map((color) => {
+      return (
+        <div
+          onClick={clickDelBtn}
+          style={{
+            backgroundColor: color,
+            borderRadius: 50,
+            height: 50,
+            width: 50,
+            margin: '0, auto, 0, auto',
+          }}>
+        </div>
+      )
+    });
+  }
+
+  render() {
 
     return (<div
       style={{
         marginTop: 60
       }}
-      onClick={clickDelBtn}
     >
-      <div style={{
-        backgroundColor: color,
-        borderRadius: 50,
-        height: 50,
-        width: 50,
-        margin: '0, auto, 0, auto',
-      }}>
-      </div>
+      {this.renderColorCircle()}
     </div>)
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import * as ReactDOM from 'react-dom';
 import ColorButton from 'components/ColorButton';
 import ColorsCode from 'components/ColorsCode';
 
+const Colors = [
+  '#e59866',
+  '#f8c471',
+  '#f7dc6f',
+  '#7dcea0',
+  '#76d7c4',
+  '#7fb3d5',
+];
+
 const App = (props) => {
 
   const redButton = {
@@ -23,11 +32,12 @@ const App = (props) => {
     clickAddBtn: () => console.info('Click'),
     color: 'yellow'
   }
+
   return (<div>
     <ColorButton {...redButton} />
     <ColorButton {...greenButton} />
     <ColorButton {...yellowButton} />
-    <ColorsCode clickDelBtn={()=> console.log('click')} color={'red'}/>
+    <ColorsCode clickDelBtn={()=> console.log('click')} colors={Colors}/>
   </div>)
 };
 
