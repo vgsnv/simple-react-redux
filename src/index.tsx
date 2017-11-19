74a39a9610194dac92a532346c16a7efdf828b2e
vgsnv <vgsnv@yandex.ru>
2017-11-19 16:45:39 +0300

    Добавляет Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,22 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-import ColorButton from 'components/ColorButton';
-import ControlPanel from 'components/ControlPanel';
-import ColorsCode from 'components/ColorsCode';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
-const ColorButtons = [
-  { id: '1', title: 'Красный', color: 'red', clickAddBtn: () => console.info('Click') },
-  { id: '2', title: 'Зеленый', color: 'green', clickAddBtn: () => console.info('Click') },
-  { id: '3', title: 'Желтый', color: 'yellow', clickAddBtn: () => console.info('Click') },
-];
+import App from 'components/App';
 
-const Colors = [
-  { id: '1', color: '#e59866' },
-  { id: '2', color: '#f8c471' },
-  { id: '3', color: '#f7dc6f' },
-  { id: '4', color: '#7dcea0' },
-  { id: '5', color: '#76d7c4' },
-  { id: '6', color: '#7fb3d5' },
-];
+const store = createStore(() => {
+  return {
+    msg: 'hello'
+  }
+});
 
-const App = (props) => {
+console.log('store', store.getState())
 
-  return (<div>
-    <ControlPanel colorButtons={ColorButtons} />
-    <ColorsCode clickDelBtn={() => console.log('click')} colors={Colors} />
-  </div>)
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ store-example.js
@@ -0,0 +1,36 @@
+// const ColorButtons = [
+//   { id: '1', title: 'Красный', color: 'red', clickAddBtn: () => console.info('Click') },
+//   { id: '2', title: 'Зеленый', color: 'green', clickAddBtn: () => console.info('Click') },
+//   { id: '3', title: 'Желтый', color: 'yellow', clickAddBtn: () => console.info('Click') },
+// ];
+
+// const Colors = [
+//   { id: '1', color: '#e59866' },
+//   { id: '2', color: '#f8c471' },
+//   { id: '3', color: '#f7dc6f' },
+//   { id: '4', color: '#7dcea0' },
+//   { id: '5', color: '#76d7c4' },
+//   { id: '6', color: '#7fb3d5' },
+// ];
+
+store = {
+
+  colorButtons: [{
+    id: '1',
+    title: 'Красный',
+    color: 'red'
+  }, {
+    id: '2',
+    title: 'Зеленый',
+    color: 'green'
+  }, {
+    id: '3',
+    title: 'Желтый',
+    color: 'yellow'
+  }],
+
+  colorsArea: [{
+    id: 'asd1',
+    color: '#e59866'
+  }]
+}
